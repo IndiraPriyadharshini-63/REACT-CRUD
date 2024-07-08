3e5356a4d89f7ad8e68310e763563272bd572c56
IndiraPriyadharshini-63 <indirapriyadharshini.mary@gmail.com>
2024-07-08 23:28:34 +0530

    navbar created

--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -24,8 +24,10 @@ function Tasks() {
       .catch((err) => console.log(err));
   };
   return (
-    <div className=" d-flex vh-100 bg-primary justify-content-center align-items-center">
+    <div className=" d-flex flex-column vh-100 bg-primary justify-content-center align-items-center">
+      <div className="d-flex ">
       <Navbar />
+      </div>
       <div className="w-50 bg-white rounded p-3 ">
         <Link to="/create" className="btn btn-primary m-2">
           Add Task
